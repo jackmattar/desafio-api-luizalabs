f15bb5109a859be204ef72babd147e2691347149
Jackeline Mattar <54188942+jackmattar@users.noreply.github.com>
2022-02-15 16:44:22 -0300

    Delete wishlists.js

--- wishlists.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const WishlistController = require('../controllers/WishlistController');
-const Auth= require('../middleweare/authentication');
-const routers = Router();
-
-routers.get('/favoritos/:id',Auth.verifyJWT,WishlistController.getList);//ok
-
-module.exports = routers;
